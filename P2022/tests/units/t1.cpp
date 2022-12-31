b0c8449f8492b900cbad271c33c801d95b451385
Ran Regev <Ran.Regev@beyeonics.com>
2022-12-31 20:02:39 +0200

    the func: implemented, does not work
    compare_three_way as the comp function
    does not compile. Not clear why

--- P2022/tests/units/t1.cpp
+++ P2022/tests/units/t1.cpp
@@ -3,8 +3,18 @@
 #include "3way.hpp"
 #include "catch2/catch_all.hpp"
 
-TEST_CASE("nop")
+TEST_CASE("t1")
 {
+    auto const v1{0, 1, 2, 3};
+    auto const v2{0, 1, 2, 4};
+
+    auto res = 
+        std::ranges::lexicographical_compare_three_way_KKK(
+            std::ranges::begin(v1), std::ranges::end(v1),
+            std::ranges::begin(v1), std::ranges::end(v1)
+        );
+
+    CHECK(res == std::strong_ordering::less);
 }
 
 
