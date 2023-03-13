21a37aa8c3d1428a552ffc4d9163952171fd3214
Alex <alex.dathskovsky@speedata.io>
2023-03-13 13:05:34 +0200

    U->Us

--- P2832/tests/header/convertible_and_same_as.hpp
+++ P2832/tests/header/convertible_and_same_as.hpp
@@ -1,22 +1,22 @@
 #include <concepts> 
 
-template <typename T, typename... U>
-concept same_as_any_of = (std::same_as<T, U> || ...);
+template <typename T, typename... Us>
+concept same_as_any_of = (std::same_as<T, Us> || ...);
 
-template <typename T, typename... U>
-concept converible_to_any_of = (std::convertible_to<T, U> || ...);
+template <typename T, typename... Us>
+concept converible_to_any_of = (std::convertible_to<T, Us> || ...);
 
-template <typename T, typename... U>
-concept converible_to_all_of = (std::convertible_to<T, U> && ...);
+template <typename T, typename... Us>
+concept converible_to_all_of = (std::convertible_to<T, Us> && ...);
 
 namespace impl{
-    template <typename T, typename... U> 
+    template <typename T, typename... Us> 
     struct convertible_counter{
-        constexpr static std::size_t counter_ = ((std::convertible_to<T, U> ? 1 : 0) + ...);
+        constexpr static std::size_t counter_ = ((std::convertible_to<T, Us> ? 1 : 0) + ...);
     };
 }
 
-template <typename T, typename... U>
-concept converible_to_one_of = impl::convertible_counter<T, U...>::counter_ == 1;
+template <typename T, typename... Us>
+concept converible_to_one_of = impl::convertible_counter<T, Us...>::counter_ == 1;
 
 
