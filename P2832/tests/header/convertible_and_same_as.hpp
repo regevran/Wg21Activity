3cc08e61c84517554eb9535a82d0742e9aa14945
Alex <alex.dathskovsky@speedata.io>
2023-03-13 08:15:36 +0200

    better concept implementation

--- P2832/tests/header/convertible_and_same_as.hpp
+++ P2832/tests/header/convertible_and_same_as.hpp
@@ -6,16 +6,17 @@ concept same_as_any_of = (std::same_as<T, U> || ...);
 template <typename T, typename... U>
 concept converible_to_any_of = (std::convertible_to<T, U> || ...);
 
+template <typename T, typename... U>
+concept converible_to_all_of = (std::convertible_to<T, U> && ...);
+
 namespace impl{
     template <typename T, typename... U> 
     struct convertible_counter{
-        constexpr static std::size_t counter_ = (static_cast<int>(std::convertible_to<T, U>) + ...);
+        constexpr static std::size_t counter_ = ((std::convertible_to<T, U> ? 1 : 0) + ...);
     };
 }
 
 template <typename T, typename... U>
 concept converible_to_one_of = impl::convertible_counter<T, U...>::counter_ == 1;
 
-template <typename T, typename... U>
-concept converible_to_all_of = impl::convertible_counter<T, U...>::counter_ == sizeof...(U);
 
