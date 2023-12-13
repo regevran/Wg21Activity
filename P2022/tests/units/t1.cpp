109d70fded4537dcc42dcc1e67611a59d00217bf
Ran Regev <Ran.Regev@beyeonics.com>
2023-12-13 11:08:33 +0200

    P2022: P2023R3.md and clean examples

--- P2022/tests/units/t1.cpp
+++ P2022/tests/units/t1.cpp
@@ -17,3 +17,16 @@ TEST_CASE("t1")
     CHECK(res == std::strong_ordering::less);
 }
 
+TEST_CASE("constexpr t1")
+{
+    constexpr std::array<int,4> v1{0, 1, 2, 3};
+    constexpr std::array<int,4> v2{0, 1, 2, 4};
+
+    constexpr auto res = 
+        std::ranges::lexicographical_compare_three_way(
+            std::ranges::begin(v1), std::ranges::end(v1),
+            std::ranges::begin(v2), std::ranges::end(v2)
+        );
+
+    static_assert(!(res != std::strong_ordering::less));
+}
